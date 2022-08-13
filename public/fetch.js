34c3ba4ba1861d39b2a4a0632b427353366e49d3
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-08-13 13:38:39 +0300

    Fetching selected coins is ready to be rendered relates #15

--- public/fetch.js
+++ public/fetch.js
@@ -1,13 +1,13 @@
-const fetch = (url, method, callback, value) => {
+const fetch = (url, method, callback) => {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = () => {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            console.log('yeah its fetching!');
+            // console.log('yeah its fetching!');
             const data = JSON.parse(xhr.responseText);
-            callback(data, value);
+            callback(data);
         }
         else {
-            console.log('Fetch Error')
+            // console.log('Fetch Error')
         }
     }
     xhr.open(method, url);
--- public/script.js
+++ public/script.js
@@ -11,6 +11,11 @@ const renderSuggestedCoins = (data) => {
         const ul = document.createElement('ul');
         ul.setAttribute('class', 'suggestions-list');
         suggestionsContainer.appendChild(ul);
+        ul.addEventListener('click', () =>{
+            // console.log(coinName.textContent);
+            const url = `https://api.coincap.io/v2/assets/${coinName.textContent.toLowerCase()}`;
+            fetch(url, 'GET', renderSelectedCoin)
+        })
 
         const li = document.createElement('li');
         li.setAttribute('class', 'suggestion-item');
@@ -26,28 +31,53 @@ const renderSuggestedCoins = (data) => {
         symbol.textContent= coin.Symbol;
         li.appendChild(symbol);
 
-        const name = document.createElement('span');
-        name.setAttribute('class', 'span');
-        name.textContent= coin.name;
-        li.appendChild(name);
+        const coinName = document.createElement('span');
+        coinName.setAttribute('class', 'span');
+        coinName.textContent= coin.name;
+        li.appendChild(coinName);
     })
 }
 
+// function test(){
+//     const suggestedCoins = Array.from(document.querySelectorAll('.suggestions-list'));
+//     if(suggestedCoins){
+//         suggestedCoins.forEach( singleSuggestion => {
+//             singleSuggestion.addEventListener('click', (e) => {
+//                 console.log(singleSuggestion);
+//                 // fetch(url, 'GET', selectCoin, e.target.value)
+//             })
+//         })
+// }
+// }
+// test();
 
-
-// const suggestedCoins = document.querySelectorAll('.single-suggestion');
-// suggestedCoins.forEach(singleSuggestion => {
-//     singleSuggestion.addEventListener('click', (e) => {
-//         fetch(url, 'GET', selectCoin, e.target.value)
-//     })
-// })
-
-// const selectCoin = (data, value) => {
-//     const selectedCoin = data.find(coin => coin.name.toLowerCase().includes(value.toLowerCase()));
-//     renderSelectedCoin(selectedCoin);
+// const selectCoin = (data) => {
+//     console.log(data,111);
+//     // const selectedCoin = data.find(coin => coin.name.toLowerCase().includes(value.toLowerCase()));
+//     // renderSelectedCoin(selectedCoin);
 // }
 
 // const renderSelectedCoin = (data) => {
 //     console.log('selected coin is ready to be rendered:', data);
+// `<div class="box">
+//             <div class="head-box">
+//                 <div class="img-title">
+//                     <img src="./assets/images.png">
+//                     <p class="name">Bitcoin<span>BTC</span></p>
+//                 </div>
+//                 <p class="percent-change-coin">5.9400 %</p>
+//             </div>
+//             <div class="content-box">
+//                 <div class="coin-details">
+//                     <p>468.2B$</p><span>Market Cap</span>
+//                 </div>
+//                 <div class="coin-details">
+//                     <p>24491.7$</p><span>Price</span>
+//                 </div>
+//                 <div class="coin-details">
+//                     <p>17.5B$</p><span>Volume</span>
+//                 </div>
+//             </div>
+//         </div>`
 // }
 
